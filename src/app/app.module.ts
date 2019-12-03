095a692823f4b84328b948b5c25228eaf033e746
LiviuLvu <liviu.iancu@3pillarglobal.com>
2019-12-03 21:05:51 +0200

    adjust components layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ParagraphComponent } from './paragraph/paragraph.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
+import { SubscribeMeComponent } from './subscribe-me/subscribe-me.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ParagraphComponent, FileUploadComponent ],
+  declarations: [ AppComponent, ParagraphComponent, FileUploadComponent, SubscribeMeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/subscribe-me/subscribe-me.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribe-me',
+  templateUrl: './subscribe-me.component.html',
+  styleUrls: ['./subscribe-me.component.css']
+})
+export class SubscribeMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
